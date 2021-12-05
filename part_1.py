23835ca56f8c62e89d77dd065d0d7e53dfa7fbd6
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2021-12-05 02:13:00 +0000

    paritally implemented mathematica code

---
+++ part_1.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_data(dat_file):
+    return np.loadtxt(dat_file)
+
+def plotChar(number_array):
+    number = number_array[1:]
+    number_matrix = np.reshape(number, (16, 16))
+    plt.title(f"The number {str(number_array[0])[0]}")
+    plt.pcolormesh(number_matrix)
+    plt.show()
+
+def kerval(a, b):    # todo: figure out what a and b are here
+    return np.dot(a, b) ^ 3
+
+def mysign(x):
+    if x <= 0:
+        return -1
+    return 1
+
+def clearGLBcls(data):                   # todo: wtf is this function, creates a big boi array?
+    return np.zeros((3, len(data)))
+
+def classPredK(dat, pat, cl):   # Compute prediction of classifier on a particular pattern
+    index = len(cl)
+    sum = 0
+    for i in range(index):
+        sum += cl[i] * kerval(pat, dat[1:])
+    return sum
+
+def trainGen(data):
+    for i in range(len(data)):
+        val = data[i][0]
+
+        # table is called with these parameters
+        # expr = classpredk[dat, Take[dat[[i]], {2, 257}]
+        # i = GLBcls[[j]]
+        # j = {j, 1, 3}
+        predictions = 0
+        GLBcls = np.zeros(3, len(data))
+        for j in range(3):
+            predictions += classPredK(data, data[1:], GLBcls[j])
+
+
+if __name__ == '__main__':
+
+    training_data = get_data('dtrain123.dat')
+    test_data = get_data('dtest123.dat')
+    print(training_data.shape)
+    print(test_data.shape)
+    plotChar(training_data[1])
+
+
