eaf59b80723cab582faa12d8f5d4357274b2f667
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2021-12-05 10:23:33 +0000

    update

--- part_1.py
+++ part_1.py
@@ -7,11 +7,13 @@ def get_data(dat_file):
 def plotChar(number_array):
     number = number_array[1:]
     number_matrix = np.reshape(number, (16, 16))
-    plt.title(f"The number {str(number_array[0])[0]}")
-    plt.pcolormesh(number_matrix)
-    plt.show()
+    # plt.title(f"The number {str(number_array[0])[0]}")
+    # plt.pcolormesh(number_matrix)
+    # plt.show()
 
 def kerval(a, b):    # todo: figure out what a and b are here
+    # a = (328, 257)
+    # b = (328, 257)
     return np.dot(a, b) ^ 3
 
 def mysign(x):
@@ -32,23 +34,31 @@ def classPredK(dat, pat, cl):   # Compute prediction of classifier on a particul
 def trainGen(data):
     for i in range(len(data)):
         val = data[i][0]
-
+        print(val)
         # table is called with these parameters
         # expr = classpredk[dat, Take[dat[[i]], {2, 257}]
         # i = GLBcls[[j]]
         # j = {j, 1, 3}
         predictions = 0
-        GLBcls = np.zeros(3, len(data))
+        GLBcls = np.zeros((3, len(data)))
         for j in range(3):
             predictions += classPredK(data, data[1:], GLBcls[j])
+        print(predictions)
 
-
+        # maxc = -10000000000000000 #ineficiency?
+        #
+        # for j in range(3):
+        #     if val == j:
+        #         y = 1
+        #     else:
+        #         y = -1
+        #     if (y * predictions[j] <= 0)
 if __name__ == '__main__':
 
     training_data = get_data('dtrain123.dat')
     test_data = get_data('dtest123.dat')
-    print(training_data.shape)
-    print(test_data.shape)
+    # print(training_data.shape)
+    # print(test_data.shape)
     plotChar(training_data[1])
-
+    trainGen(training_data)
 
