3e7d4071acee03fac74aa16659ba150143168e5a
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2021-12-14 05:10:21 +0000

    some more plots

---
+++ part_3_1nn_vect_multi.py
@@ -0,0 +1,57 @@
+from concurrent.futures import ProcessPoolExecutor
+
+import numpy as np
+import part_3_perceptron as perceptron
+import matplotlib.pyplot as plt
+# from concurrent import futures
+from tqdm import tqdm
+import os
+# from line_profiler_pycharm import profile
+
+np.random.seed(0)
+
+
+# @profile
+def concurrent_optimised_sample_complexity(dimension):
+    dimension_test = tuple()
+    generalisation_error = 1
+    sample_count = 1
+    while generalisation_error >= 0.1:
+        training_dataset, testing_dataset = perceptron.setup(dimension, sample_count)
+        mistakes = 0
+        for i in range(len(testing_dataset)):
+            prediction = training_dataset[
+                np.argmin(np.linalg.norm(testing_dataset[i][:-1] - training_dataset[:, :-1], axis=1))
+            ][-1]
+            if prediction != testing_dataset[i][-1]:
+                mistakes += 1
+        generalisation_error = mistakes / len(testing_dataset)
+        dimension_test = (dimension, sample_count, generalisation_error)
+        sample_count += 1
+    # optimisation.append(dimension_test)
+    return dimension_test
+
+
+if __name__ == '__main__':
+    if not os.path.exists('plots'):
+        os.makedirs('plots')
+
+    # complexity = 9
+    complexity = 20
+    optimisation = list(tuple())
+    p_bar = tqdm(smoothing=1)
+    ppe = ProcessPoolExecutor(max_workers=2)
+    for result in ppe.map(concurrent_optimised_sample_complexity, range(1, complexity + 1)):
+        optimisation.append(result)
+        p_bar.update(1)
+
+    optimisation.sort(key=lambda x: x[0])
+    print(optimisation)
+
+    n_values = [n[0] for n in optimisation]
+    m_values = [m[1] for m in optimisation]
+    plt.plot(n_values, m_values)
+    plt.title(f"1-NN generalisation error optimisation up to dimensionality of {str(complexity)}")
+    plt.xlabel("dimension")
+    plt.ylabel("sample size")
+    plt.savefig(f'./plots/part3_a_1nn_{str(complexity)}_vect_mult.png')
--- part_3_1nn_vectorised.py
+++ part_3_1nn_vectorised.py
@@ -43,7 +43,7 @@ def nearest_neighbour_vectorized(sample, dataset):
     return dataset[np.argmin(np.linalg.norm(sample[:-1] - dataset[:, :-1], axis=1))]
 
 
-def concurrent_optimised_sample_complexity(dimension):
+def concurrent_optimised_sample_complexity(dimension, count):
     dimension_test = tuple()
     generalisation_error = 1
     sample_count = 1
@@ -59,7 +59,7 @@ def concurrent_optimised_sample_complexity(dimension):
                 mistakes += 1
         generalisation_error = mistakes / len(testing_dataset)
         dimension_test = (dimension, sample_count, generalisation_error)
-        sample_count += 5
+        sample_count += count
     optimisation.append(dimension_test)
 
 
@@ -68,16 +68,11 @@ if __name__ == '__main__':
         os.makedirs('plots')
 
     complexity = 20
+    sample_count = 2
     optimisation = list(tuple())
     p_bar = tqdm(smoothing=1)
-    # with futures.ThreadPoolExecutor(max_workers=1) as executor:
-    #     futures_to_jobs = {executor.submit(concurrent_optimised_sample_complexity, job): job for job in
-    #                        range(complexity + 1)}
-    #     for _ in futures.as_completed(futures_to_jobs):
-    #         p_bar.update(1)
-    #         p_bar.refresh()
     for job in range(1, complexity + 1):
-        concurrent_optimised_sample_complexity(job)
+        concurrent_optimised_sample_complexity(job, sample_count)
         p_bar.update(1)
 
     optimisation.sort(key=lambda x: x[0])
@@ -86,7 +81,7 @@ if __name__ == '__main__':
     n_values = [n[0] for n in optimisation]
     m_values = [m[1] for m in optimisation]
     plt.plot(n_values, m_values)
-    plt.title(f"1-NN generalisation error optimisation up to dimensionality of {str(complexity)}")
+    plt.title(f"1-NN generalisation error up to dimensionality of {str(complexity)}")
     plt.xlabel("dimension")
     plt.ylabel("sample size")
-    plt.savefig(f'./plots/part3_a_1nn_{str(complexity)}_vectorised.png')
+    plt.savefig(f'./plots/part3_a_1nn_{str(complexity)}_vectorised_{str(sample_count)}.png')
