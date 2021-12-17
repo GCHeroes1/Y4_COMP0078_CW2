9971bf4be5be067787ca8a76af238dcfa43f84c2
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2021-12-17 16:22:46 +0000

    not any closer to solving clustering problem

--- part_2_clustering.py
+++ part_2_clustering.py
@@ -12,16 +12,58 @@ def random_sample(n, l):
     data = np.random.rand(1, l*n)
     return np.reshape(data, (l, n))
 
-def weight(c, x_, l):
+def weight(c, x_):
+    l = len(x_)
     # c is a constant, x is a matrix, l is len(x)
     W = np.zeros((l, l))
     for i_ in range(l):
         for j in range(l):
-            W[i_][j] = np.exp(-c * np.linalg.norm(np.subtract(x_[i_], x_[j])))
+            W[i_][j] = np.exp(-c * np.linalg.norm((x_[i_], x_[j])))
     return W
 
-def vectorised_weight(c, x):
+# def norm(x, i, j):
+#     return np.sqrt(np.sum(x[i] - x[j])**2)
+#     # difference = np.subtract(x[i], x[j])
+#     # square_difference = np.square(difference)
+#     # return square_difference
+#
+# def weight_2(c, x_):
+#     l = len(x_)
+#     # c is a constant, x is a matrix, l is len(x)
+#     W = np.zeros((l, l))
+#     for i_ in range(l):
+#         for j in range(l):
+#             W[i_][j] = np.exp(-c * np.linalg.norm((x_[i_], x_[j])))
+#             # W[i_][j] = np.exp(-c * np.linalg.norm((x_[i_], x_[j])))
+#     return W
+#
+# def weight_3(c, x_):
+#     l = len(x_)
+#     # c is a constant, x is a matrix, l is len(x)
+#     W = np.zeros((l, l))
+#     for i_ in range(l):
+#         for j in range(l):
+#             W[i_][j] = np.exp(-c * euclidean(x_[i_], x_[j])**2)
+#             # W[i_][j] = np.exp(-c * np.linalg.norm((x_[i_], x_[j])))
+#     return W
+
+def weight_4(c, x_):
+    l = len(x_)
     # c is a constant, x is a matrix, l is len(x)
+    W = np.zeros((l, l))
+    for i_ in range(l):
+        for j in range(l):
+            W[i_][j] = np.exp(-c * distance(x_[i_], x_[j]))
+            # W[i_][j] = np.exp(-c * np.linalg.norm((x_[i_], x_[j])))
+    return W
+
+def distance(x, y):
+    return (np.sum((x - y) ** 2)) ** 0.5
+
+def vectorised_weight(c, x): # seems wrong
+    # print(x)
+    # c is a constant, x is a matrix, l is len(x)
+    # W = np.exp(-c * np.square(np.mod(x[np.newaxis, :, :] - x[:, np.newaxis, :]), axis=2))
     W = np.exp(-c * np.linalg.norm(x[np.newaxis, :, :] - x[:, np.newaxis, :], axis=2))
     return W
 
@@ -45,7 +87,6 @@ def sign(x_):
     return 0
 
 def eigenvalues_sorted(laplacian_):
-    eval, evec = np.linalg.eig(laplacian_)
     eigenValues, eigenVectors = np.linalg.eig(laplacian_)
 
     idx = eigenValues.argsort()[::-1]
@@ -55,15 +96,20 @@ def eigenvalues_sorted(laplacian_):
 
 def clustering(c, x_):               # c is an unknown constant, x is the data matrix (lxl sized)
     c = 2**c
-    weight_matrix = vectorised_weight(c, x_)                         # should be correct
+    weight_matrix = weight(c, x_)                         # should be correct, i tested this so fucking much
     # print(weight_matrix)
-    diagonal_matrix = vectorised_diagonalD(weight_matrix)              # should be correct
+    diagonal_matrix = diagonalD(weight_matrix)              # should be correct, tested with unvectorised and vectorised, should really be correct
     # print(diagonal_matrix[1][1])
     laplacian = diagonal_matrix - weight_matrix                         #must be correct
     eigenspace = np.linalg.eig(laplacian)                               # correct
-    eigenspace_2 = eigenvalues_sorted(laplacian)
-    # v_2 = (eigenspace[1][np.argsort(eigenspace[0])[1]])                 # this has to be correct but something is wrong?? it doesnt do any better than just eigenspace[1][1]
-    v_2 = eigenspace_2[1][1]
+    # v_2 = eigenspace[1][1]
+    v_2 = (eigenspace[1][np.argsort(eigenspace[0])[1]])                 # this has to be correct but something is wrong?? it doesnt do any better than just eigenspace[1][1] but it performs worse than the line above
+    # eigenspace_2 = eigenvalues_sorted(laplacian)                        # correct?
+    # v_2 = eigenspace_2[1][1]
+    # plt.scatterplot(range(len(eigenspace[0])), eigenspace[0])
+    # plt.show()
+    # plt.clf()
+    # v_2 = eigenspace_2[1][1]
     # print(np.argsort(eigenspace[0])[1])
     # print(np.round(eigenspace[0][0:5]))
     # v_3 = eigenspace[1][1]
@@ -88,11 +134,11 @@ def clustering(c, x_):               # c is an unknown constant, x is the data m
     # v_2 = eigenspace[1][4]
 
     cluster = np.zeros(len(v_2))
-    for z in range(len(v_2)):
-        if sign(v_2[z]) == 0 or sign(v_2[z]) == 1:
-            cluster[z] = 1
+    for i in range(len(v_2)):
+        if sign(v_2[i]) == 0 or sign(v_2[i]) == 1:
+            cluster[i] = 1
         else:
-            cluster[z] = -1
+            cluster[i] = -1
         # cluster[z] = sign(v_2[z])
     return cluster
 
@@ -111,7 +157,8 @@ def clustering(c, x_):               # c is an unknown constant, x is the data m
 def c_values(limits, step):
     return np.round(np.arange(-limits, limits, step), 1)
 
-def correct_classification(X):
+def correct_classification():
+    X = get_data('twomoons.dat')
     for i in range(len(X)):
         temp_marker, temp_color = marker[1], color[1]
         if np.round(dataset[i][0]) == 1:
@@ -146,10 +193,11 @@ def plot_cluster(c, data):
 
 def calculate_cluster_error(c_value_, dataset_):
     data_ = dataset_[:, 1:]
+    labels = dataset_[:, :1]
     cluster_ = clustering(c_value_, data_)
     mistakes_ = 0
     for z in range(len(cluster_)):
-        if cluster_[z] != np.round(dataset_[z][0], 1):
+        if cluster_[z] != labels[z]:
             mistakes_ += 1
     generalisation_error_ = (mistakes_ / len(cluster_))
     return generalisation_error_, c_value_
@@ -166,21 +214,23 @@ if __name__ == '__main__':
     # X = dataset[:, 1:]
     # labels = dataset[:, :1]
 
-    c_array = c_values(500, 0.1)
-
-    # generalisations = list(tuple())
-    # for x in range((len(c_array))):
-    #     cluster = clustering(c_array[x], dataset[:, 1:])
-    #     mistakes = 0
-    #     for i in range(len(cluster)):
-    #         if cluster[i] != np.round(dataset[i][0]):
-    #             mistakes += 1
-    #     generalisation_error = (mistakes / len(cluster))
-    #     generalisations.append((generalisation_error, c_array[x]))
-    # # print(generalisations)
-    # print(min(generalisations))
+    c_array = c_values(20, 0.2)
 
     dataset = get_data('twomoons.dat')
+    generalisations = list(tuple())
+    for x in range((len(c_array))):
+        cluster = clustering(c_array[x], dataset[:, 1:])
+        mistakes = 0
+        for i in range(len(cluster)):
+            if cluster[i] != np.round(dataset[i][0]):
+                mistakes += 1
+        generalisation_error = (mistakes / len(cluster))
+        generalisations.append((generalisation_error, c_array[x]))
+    # print(generalisations)
+    print(min(generalisations))
+
+    # multiprocess uncomment bellow
+    # dataset = get_data('twomoons.dat')
     # generalisations_ = list(tuple())
     # p_bar_ = tqdm(smoothing=1)
     # ppe = ProcessPoolExecutor(max_workers=4)
@@ -192,4 +242,4 @@ if __name__ == '__main__':
     # print(generalisations)
     # print(min(generalisations_))
 
-    plot_cluster(-29.9, dataset[:, 1:])
+    # plot_cluster(-29.9, dataset[:, 1:])
